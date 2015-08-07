05fdd6c22c6089a0ceb7e652cb433243d9dd33d4
Yao Lu <yaolu@tibco-support.com>
2015-08-07 16:14:34 +0800

    first commit

---
+++ higher_order_functions.hs
@@ -0,0 +1,33 @@
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+subtractFromFour :: (Num a) => a -> a
+subtractFromFour = (subtract 4)
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+	where g x y = f y x
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs 
---
+++ list_comprehension.hs
@@ -0,0 +1,7 @@
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+
+removeOdds xxs = [ [ x | x <- xs, even x ] | xs <- xxs ]
