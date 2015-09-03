7f933179eeaf576d17dc3347c6cc3e6a93603196
Yao Lu <yaolu@tibco-support.com>
2015-09-03 17:19:45 +0800

    Fixed an issue in random_test.hs

--- input_and_output/random_test.hs
+++ input_and_output/random_test.hs
@@ -11,14 +11,7 @@ threeCoins gen =
 randoms' :: (RandomGen g, Random a) => g -> [a]
 randoms' gen = let (value, newGen) = random gen in value:randoms' newGen
 
---finiteRandoms :: (RandomGen g, Random a, Num n) => n -> g -> ([a], g)
---finiteRandoms 0 gen = ([], gen)
---finiteRandoms n gen =
---    let (value, newGen) = random gen
---        (restOfList, finalGen) = finiteRandoms (n-1) newGen
---    in  (value:restOfList, finalGen)
-
-finiteRandoms :: (RandomGen g, Random a, Num n) => n -> g -> ([a], g)  
+finiteRandoms :: (RandomGen g, Random a, Integral n) => n -> g -> ([a], g)  
 finiteRandoms 0 gen = ([], gen)  
 finiteRandoms n gen =   
     let (value, newGen) = random gen  
