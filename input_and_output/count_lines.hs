95d51cf538ce244f353a64a536542fae5fcaac0b
Yao Lu <yaolu@tibco-support.com>
2015-09-07 10:56:30 +0800

    Fixed a deprecated function name

--- input_and_output/count_lines.hs
+++ input_and_output/count_lines.hs
@@ -1,15 +1,17 @@
-import System.Environment  
-import System.IO  
-import System.IO.Error  
-  
-main = toTry `catch` handler  
-              
-toTry :: IO ()  
-toTry = do (fileName:_) <- getArgs  
-           contents <- readFile fileName  
-           putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"  
-  
-handler :: IOError -> IO ()  
-handler e  
-    | isDoesNotExistError e = putStrLn "The file doesn't exist!"  
+import System.Environment     
+import System.IO     
+import System.IO.Error     
+    
+main = toTry `catchIOError` handler     
+                 
+toTry :: IO ()     
+toTry = do (fileName:_) <- getArgs     
+           contents <- readFile fileName     
+           putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"     
+    
+handler :: IOError -> IO ()     
+handler e     
+    | isDoesNotExistError e =   
+        case ioeGetFileName e of Just path -> putStrLn $ "Whoops! File does not exist at: " ++ path  
+                                 Nothing -> putStrLn "Whoops! File does not exist at unknown location!"  
     | otherwise = ioError e
